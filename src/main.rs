6621bf5df6843945ae32f6a4bb68d1620de6d4ab
Bauke Westendorp <baukewestendorp@gmail.com>
2026-01-20 18:21:15 +0100

    Copy controller example from zeevonk

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use zeevonk::client::controller::Client;
+use zeevonk::ident::Identifier;
+use zeevonk::trigger::{Trigger, TriggerValue};
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let mut client = Client::new(Identifier::new("zv-example-controller").unwrap());
+    client.connect("ws://127.0.0.1:7335").await.unwrap();
+
+    loop {
+        client
+            .send_trigger(Trigger::new(
+                Identifier::new("button-1").unwrap(),
+                TriggerValue::Boolean(true),
+            ))
+            .await
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_secs_f32(1.0 / 10.0));
+    }
+}
